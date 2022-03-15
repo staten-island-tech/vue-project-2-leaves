fcbe6275fdd9fa54c893cc7ed112ef9e87d09954
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2022-03-15 10:44:51 -0400

    Update index.vue

--- project/pages/index.vue
+++ project/pages/index.vue
@@ -1,26 +1,50 @@
 <template>
   <div class="hi">
-    <h1 class="g">hi</h1>
+    <div class="title">
+      <h1 class="g">Galar Region</h1>
+    </div>
+    <div class="directory">
+      <button>About</button>
+      <button>Town</button>
+      <button>Wild Area</button>
+      <button>Gyms</button>
+      <button>Dynmax</button>
+      <button>Theme</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   components: {},
-  
-}
+};
 </script>
 
 <style>
-  @import '../styles/root.css';
-  body, head, * {
-    font-size: 10px;
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-  .hi {
-    height: 100vh;
-    background-color: var(--lightblue);
-  }
+@import "../styles/root.css";
+body,
+head,
+* {
+  font-size: 10px;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.title {
+  height: 6vh;
+  background: #3f0000;
+  text-align: center;
+  color: aliceblue;
+}
+h1 {
+  font-size: var(--extraLarge);
+}
+.directory {
+  background-color: var(--lightblue);
+  height: 5vh;
+  text-align: center;
+}
+button {
+  font-size: var(--h2);
+}
 </style>
