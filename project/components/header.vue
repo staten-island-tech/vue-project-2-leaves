7823277bef5f40be939f1642c970e3ace92d69c3
highonsucrose <90789920+highonsucrose@users.noreply.github.com>
2022-03-15 13:11:12 -0400

    add header
    progress on header, adjusted file organization again

--- project/components/header.vue
+++ project/components/header.vue
@@ -1,15 +1,56 @@
 <template>
   <div class="header">
-      <div class="navigation"></div>
+    <div class="title">
+      <h1 class="g">Galar Region</h1>
+    </div>
+    <div class="directory">
+      <button class="button">About</button>
+      <button class="button">Town</button>
+      <button class="button">Wild Area</button>
+      <button class="button">Gyms</button>
+      <button class="button">Dynmax</button>
+      <button class="button">Theme</button>
+    </div>
   </div>
+  <!-- Note: this is for the header please don't touch -->
 </template>
 
 <script>
 export default {
-
+    name: "Header",
+       
 }
 </script>
 
 <style>
+.title {
+  background: #3f0000;
+  text-align: center;
+  
+}
+.g {
+  font-size: var(--extraLarge);
+  color: var(--lightblue);
+}
+.directory {
+  background-color: var(--lightblue);
+  text-align: center;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+}
+.button {
+  flex-basis: 8%;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  background-color: transparent;
+  border: none;
+  font-size: var(--h4)
+}
+.button:hover {
+  transform: scaleX(1.1);
+}
 
 </style>
--- project/pages/index.vue
+++ project/pages/index.vue
@@ -1,25 +1,17 @@
 <template>
-  <div class="hi">
-    <div class="title">
-      <h1 class="g">Galar Region</h1>
-    </div>
-    <div class="directory">
-      <button>About</button>
-      <button>Town</button>
-      <button>Wild Area</button>
-      <button>Gyms</button>
-      <button>Dynmax</button>
-      <button>Theme</button>
-    </div>
+  <div class="site">
+    <Header></Header>
   </div>
 </template>
-
 <script>
+import Header from "../components/header.vue"
 export default {
-  components: {},
+  components: {
+    Header,
+  },
+  
 };
 </script>
-
 <style>
 @import "../styles/root.css";
 body,
@@ -30,23 +22,5 @@ head,
   margin: 0;
   box-sizing: border-box;
 }
-.title {
-  height: 6vh;
-  background: #3f0000;
-  text-align: center;
-  color: aliceblue;
-}
-h1 {
-  font-size: var(--extraLarge);
-}
-.directory {
-  background-color: var(--lightblue);
-  height: 5vh;
-  text-align: center;
-}
-button {
-  font-size: var(--h2);
-  margin: 1rem;
-  background-color: var(--lightblue);
-}
+
 </style>
