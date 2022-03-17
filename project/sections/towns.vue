2f899f22db78620433453311f8dc07da409c418a
highonsucrose <90789920+highonsucrose@users.noreply.github.com>
2022-03-17 13:11:56 -0400

    nsadgfds
    qaf

--- project/sections/towns.vue
+++ project/sections/towns.vue
@@ -1,12 +1,42 @@
 <template>
     <div class="towns">
         <h4 class="sectionTitle">Towns</h4>
+        <Card></Card>
     </div>
 </template>
 
 <script>
+import Card from '../components/card.vue'
 export default {
     name: "Towns",
+    components: {
+        Card,
+    },
+    data() {
+        return {
+            towns: [
+                {
+                    name: "Stow-on-Side",
+                    image: "",
+                    hasGym: true,
+                    desc: "",
+                    route: [] //<-num array of routes, should we keep it this way or just write the routes in the description?
+                },
+                {
+                    name: "Motostoke",
+                    image: "",
+                    hasGym: true,
+                    desc: "",
+                },
+                {
+                    name: "Wedgehurst",
+                    image: "",
+                    hasGym: true,
+                    desc: "",
+                }
+            ],
+        }
+    },
 }
 </script>
 
